bac38e4ef259eba10d3d9cd47d1c81f7fc628e16
joounii <95225214+joounii@users.noreply.github.com>
2025-11-03 11:12:13 +0100

    fixed imports

--- frontend/frontend/src/pages/index.ts
+++
@@ -1,9 +0,0 @@
-// src/pages/index.ts
-export * from "./BasicOpsPanel";
-export * from "./AdvancedMathPanel";
-export * from "./UnitsPanel";
-export * from "./ExpressionPanel";
-export * from "./HistoryPanel";
-export * from "./AISolverPanel";
-export * from "./SettingsPanel";
-export * from "./ProfilePanel";
